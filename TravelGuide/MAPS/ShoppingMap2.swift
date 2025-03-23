f5debeb1783c325c9da2ba6e418b59a39ea24bb7
angkumar <angkumar787@gmail.com>
2025-03-23 10:30:14 -0400

    new Maps added

--- TravelGuide/MAPS/ShoppingMap2.swift
+++ TravelGuide/MAPS/ShoppingMap2.swift
@@ -6,11 +6,72 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct ShoppingMap2: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct ShoppingMap2: UIViewRepresentable {
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: ShoppingMap2
+        
+        init(parent: ShoppingMap2) {
+            self.parent = parent
+        }
+        
+        // Render polyline for real road route
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            if let polyline = overlay as? MKPolyline {
+                let renderer = MKPolylineRenderer(polyline: polyline)
+                renderer.strokeColor = UIColor.red
+                renderer.lineWidth = 5
+                return renderer
+            }
+            return MKOverlayRenderer()
+        }
     }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+        let startCoordinate = CLLocationCoordinate2D(latitude: 42.485984063745754, longitude: -83.53735398714915) // Home
+        let endCoordinate = CLLocationCoordinate2D(latitude: 42.49188857396703, longitude: -83.47064573617806) // Walmart
+        
+        // Add markers (annotations)
+        let locations = [
+            ("Twelve Oaks Mall", endCoordinate),
+            ("Home", startCoordinate),
+        ]
+        
+        for location in locations {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location.1
+            annotation.title = location.0
+            mapView.addAnnotation(annotation)
+        }
+        
+        // Request real road route
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: startCoordinate))
+        request.destination = MKMapItem(placemark: MKPlacemark(coordinate: endCoordinate))
+        request.transportType = .automobile
+        
+        let directions = MKDirections(request: request)
+        directions.calculate { response, error in
+            if let route = response?.routes.first {
+                mapView.addOverlay(route.polyline)
+                
+                // Adjust map region to fit the route
+                mapView.setVisibleMapRect(route.polyline.boundingMapRect, edgePadding: UIEdgeInsets(top: 50, left: 50, bottom: 50, right: 50), animated: true)
+            }
+        }
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {}
 }
 
 #Preview {
--- TravelGuide/Places/Shopping2.swift
+++ TravelGuide/Places/Shopping2.swift
@@ -10,28 +10,42 @@ import MapKit
 
 struct Shopping2: View {
     var body: some View {
-        ScrollView {
-            Text("Twelve Oaks Mall")
-                .font(.largeTitle)
-                .padding()
-            
-            Text("This is the best place to waste thousands of dollars on anything you want/'need'")
-            
-            Image("tom")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(20)
-                .frame(width: 400, height: 300)
-            
-            Text("Twelve Oaks Mall is a shopping mall with over 180 stores which is located in Novi, Michigan, United States, a suburb of Detroit. The mall features anchor stores JCPenney, Macy's, and Nordstrom, with two vacant anchors previously Lord & Taylor and Sears")
+        NavigationView{
+            ScrollView {
+                Text("Twelve Oaks Mall")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Text("This is the best place to waste thousands of dollars on anything you want/'need'")
+                
+                Image("tom")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(20)
+                    .frame(width: 400, height: 300)
+                
+                Text("Twelve Oaks Mall is a shopping mall with over 180 stores which is located in Novi, Michigan, United States, a suburb of Detroit. The mall features anchor stores JCPenney, Macy's, and Nordstrom, with two vacant anchors previously Lord & Taylor and Sears")
 
-            Map() {
-                Marker("Twele oaks Mall", coordinate: CLLocationCoordinate2D(latitude: 42.49188857396703, longitude :-83.47064573617806))
+                Map() {
+                    Marker("Twele oaks Mall", coordinate: CLLocationCoordinate2D(latitude: 42.49188857396703, longitude :-83.47064573617806))
+                }
+                .frame(height: 300)
+                .cornerRadius(20)
+                .mapStyle(.hybrid)
+                
+                NavigationLink(destination: ShoppingMap2().edgesIgnoringSafeArea(.all)) {
+                    Text("Click here for directions.")
+                        .fontWeight(.heavy)
+                        .font(.title3)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(LinearGradient(gradient: Gradient(colors: [.pink, .purple]), startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(40)
+                }
+
+                
             }
-            .frame(height: 300)
-            .cornerRadius(20)
-            .mapStyle(.hybrid)
-            
         }
     }
 }
