49471084f2144269f38e9fcc9f5ce66f961f903b
angkumar <angkumar787@gmail.com>
2025-04-15 21:24:53 -0400

    new stuff

--- TravelGuide/Places/Home.swift
+++ TravelGuide/Places/Home.swift
@@ -17,7 +17,7 @@ struct Home: View {
                     .font(.largeTitle)
                     .padding()
                 
-                Text("This is the place you call home. Home is where friends and family are at.")
+                Text("This is the place you call home. Home is where friends and family are at. Home is home.")
                 
                 Image("home")
                     .resizable()
